6e97b91352a306ecc4f04f2cb85d6da045da7ff3
FSavareses <franco_savareses_lsf@hotmail.com>
2023-01-04 09:48:14 -0300

    CamelCase Problem

--- app/src/test/java/com/example/hackerrank/ExampleUnitTest.kt
+++ app/src/test/java/com/example/hackerrank/ExampleUnitTest.kt
@@ -1,33 +1,39 @@
 package com.example.hackerrank
+
 /**
 -- Problem --
+There is a sequence of words in CamelCase as a string of letters,S, having the following properties:
 
+It is a concatenation of one or more words consisting of English letters.
+All letters in the first word are lowercase.
+For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+Given S, determine the number of words in S.
 
 Example:
+input = saveChangesInTheEditor:
 
+output = 5
+
+save
+Changes
+In
+The
+Editor
  */
 
 fun main() {
-   val escalera = 6
-   println(crearEscalera(escalera))
+    val cadena = "camelCaseProblem"
+    println(camelcase(cadena))
 }
 
-fun crearEscalera(escalera: Int) {
-   var max = escalera
-   var txt = ""
-   for (x in 1..escalera) {
-
-      for (i in 2..max) {
-         print(" ")
-
-
-      }
-      for (z in 1.. (x)) {
-
-         print("#")
-      }
-      println("")
-      max--
-
-   }
+fun camelcase(cadena: String): Int {
+    var palabras = 0
+    if (cadena.length > 0) {
+        palabras++
+        for (x in cadena) {
+            if (x.isUpperCase()) palabras++
+        }
+    }
+    return palabras
 }
+
